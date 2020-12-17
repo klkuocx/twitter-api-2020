244a4e34dd8e6a7477f8586ad6a940b55f756477
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-17 12:38:12 +0800

    POST /api/tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -30,7 +30,25 @@ const tweetController = {
         return res.json({ status: 'failure', message: 'Errors' })
       })
   },
-  postTweet: (req, res) => {}
+  postTweet: (req, res) => {
+    const { description } = req.body
+    if (!description) {
+      return res.json({ status: 'failure', message: "description didn't exist" })
+    } else if (description.length > 140) {
+      return res.json({ status: 'failure', message: 'number of the words must between 1 ~ 140' })
+    } else {
+      Tweet.create({
+        description: description,
+        UserId: helpers.getUser(req).id
+      })
+        .then(tweet => {
+          return res.json({ status: 'success', message: 'tweet was successfully created', tweet })
+        })
+        .catch(err => {
+          return res.json({ status: 'failure', message: 'Errors' })
+        })
+    }
+  }
 }
 
 module.exports = tweetController
