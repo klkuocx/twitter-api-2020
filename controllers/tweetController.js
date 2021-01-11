b28c85a952232396f17aea4ac07ff74d2c4237f3
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-11 18:48:46 +0800

    refactor: modify tweetController by async/await

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -7,120 +7,122 @@ const Reply = db.Reply
 const Like = db.Like
 
 const tweetController = {
-  readTweets: (req, res, next) => {
-    const userId = helpers.getUser(req).id
-    Tweet.findAll({
-      options: {
-        attribute: ['id', 'UserId', 'description', 'createdAt', 'updatedAt']
-      },
-      order: [['createdAt', 'DESC']],
-      include: [
-        { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
-        { model: Like, attributes: ['UserId'] },
-        { model: Reply, attributes: ['UserId'] }
-      ]
-    })
-      .then(tweets => {
-        tweets = tweets.map(tweet => ({
-          ...Object.keys(tweet.dataValues).reduce((result, key) => {
-            result[key] = tweet[key]
-            return result
-          }, {}),
-          isLiked: tweet.Likes.map(l => l.UserId).includes(userId),
-          repliedCount: tweet.Replies.length,
-          LikeCount: tweet.Likes.length,
-          isSelf: tweet.UserId === userId
-        }))
-        return res.json(tweets)
+  readTweets: async (req, res, next) => {
+    try {
+      const userId = helpers.getUser(req).id
+      const tweets = await Tweet.findAll({
+        options: {
+          attribute: ['id', 'UserId', 'description', 'createdAt', 'updatedAt']
+        },
+        order: [['createdAt', 'DESC']],
+        include: [
+          { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
+          { model: Like, attributes: ['UserId'] },
+          { model: Reply, attributes: ['UserId'] }
+        ]
       })
-      .catch(next)
-  },
-  postTweet: (req, res, next) => {
-    const userId = helpers.getUser(req).id
-    const { description } = req.body
-    if (!description || description.length > 140) {
-      return res.status(400).json({ message: 'number of the words must between 1 ~ 140' })
-    } else {
-      Tweet.create({
-        description: description,
-        UserId: userId
-      })
-        .then(tweet => {
-          return res.json({ message: 'tweet is successfully created', tweet })
-        })
-        .catch(next)
+      await tweets.map(tweet => ({
+        ...Object.keys(tweet.dataValues).reduce((result, key) => {
+          result[key] = tweet[key]
+          return result
+        }, {}),
+        isLiked: tweet.Likes.map(l => l.UserId).includes(userId),
+        repliedCount: tweet.Replies.length,
+        LikeCount: tweet.Likes.length,
+        isSelf: tweet.UserId === userId
+      }))
+      return res.json(tweets)
+    } catch (err) {
+      next()
     }
   },
-  readTweet: (req, res, next) => {
-    const tweetId = req.params.id
-    const userId = helpers.getUser(req).id
-    Tweet.findByPk(tweetId, {
-      options: {
-        attribute: ['id', 'UserId', 'description', 'createdAt', 'updatedAt']
-      },
-      include: [
-        { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
-        { model: Reply, attributes: ['UserId'] },
-        { model: Like, attributes: ['UserId'] }
-      ]
-    })
-      .then(tweet => {
-        if (!tweet) {
-          return res.status(400).json({ message: 'tweet not exist' })
-        }
-        tweet = {
-          ...Object.keys(tweet.dataValues).reduce((result, key) => {
-            result[key] = tweet[key]
-            return result
-          }, {}),
-          isLiked: tweet.Likes.map(l => l.UserId).includes(userId),
-          repliedCount: tweet.Replies.length,
-          LikeCount: tweet.Likes.length,
-          isSelf: tweet.UserId === userId
-        }
-        return res.json(tweet)
-      })
-      .catch(next)
-  },
-  updateTweet: (req, res, next) => {
-    const tweetId = req.params.id
-    const userId = helpers.getUser(req).id
-    const { description } = req.body
-    if (!description || description.length > 140) {
-      return res.status(400).json({ message: 'number of the words must between 1 ~ 140' })
-    }
-    Tweet.findByPk(tweetId)
-      .then(tweet => {
-        if (!tweet) {
-          return res.status(400).json({ message: 'tweet not exist' })
-        }
-        if (tweet.UserId !== userId) {
-          return res.status(403).json({ message: 'permission denied' })
-        } else {
-          return tweet.update({ description }).then(tweet => {
-            res.json({ message: 'Tweet is updated successfully', tweet })
-          })
-        }
-      })
-      .catch(next)
-  },
-  deleteTweet: (req, res, next) => {
-    const tweetId = req.params.id
-    const userId = helpers.getUser(req).id
-    Tweet.findByPk(tweetId)
-      .then(tweet => {
-        if (!tweet) {
-          return res.status(400).json({ message: 'tweet not exist' })
-        }
-        if (tweet.UserId !== userId) {
-          return res.status(403).json({ message: 'permission denied' })
-        }
-        return tweet.destroy().then(tweet => {
-          res.json({ message: 'Tweet is delete successfully', tweet })
+  postTweet: async (req, res, next) => {
+    try {
+      const userId = helpers.getUser(req).id
+      const { description } = req.body
+      if (!description || description.length > 140) {
+        return res.status(400).json({ message: 'number of the words must between 1 ~ 140' })
+      } else {
+        const tweet = await Tweet.create({
+          description: description,
+          UserId: userId
         })
+        return res.json({ message: 'tweet is successfully created', tweet })
+      }
+    } catch (err) {
+      next()
+    }
+  },
+  readTweet: async (req, res, next) => {
+    try {
+      const tweetId = req.params.id
+      const userId = helpers.getUser(req).id
+      let tweet = await Tweet.findByPk(tweetId, {
+        options: {
+          attribute: ['id', 'UserId', 'description', 'createdAt', 'updatedAt']
+        },
+        include: [
+          { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
+          { model: Reply, attributes: ['UserId'] },
+          { model: Like, attributes: ['UserId'] }
+        ]
       })
-      .catch(next)
+      if (!tweet) {
+        return res.status(400).json({ message: 'tweet not exist' })
+      }
+      tweet = {
+        ...Object.keys(tweet.dataValues).reduce((result, key) => {
+          result[key] = tweet[key]
+          return result
+        }, {}),
+        isLiked: tweet.Likes.map(l => l.UserId).includes(userId),
+        repliedCount: tweet.Replies.length,
+        LikeCount: tweet.Likes.length,
+        isSelf: tweet.UserId === userId
+      }
+      return res.json(tweet)
+    } catch (err) {
+      next()
+    }
+  },
+  updateTweet: async (req, res, next) => {
+    try {
+      const tweetId = req.params.id
+      const userId = helpers.getUser(req).id
+      const { description } = req.body
+      if (!description || description.length > 140) {
+        return res.status(400).json({ message: 'number of the words must between 1 ~ 140' })
+      }
+      let tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) {
+        return res.status(400).json({ message: 'tweet not exist' })
+      }
+      if (tweet.UserId !== userId) {
+        return res.status(403).json({ message: 'permission denied' })
+      } else {
+        tweet = await tweet.update({ description })
+        return res.json({ message: 'Tweet is updated successfully', tweet })
+      }
+    } catch (err) {
+      next()
+    }
+  },
+  deleteTweet: async (req, res, next) => {
+    try {
+      const tweetId = req.params.id
+      const userId = helpers.getUser(req).id
+      let tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) {
+        return res.status(400).json({ message: 'tweet not exist' })
+      }
+      if (tweet.UserId !== userId) {
+        return res.status(403).json({ message: 'permission denied' })
+      }
+      tweet = await tweet.destroy()
+      return res.json({ message: 'Tweet is delete successfully', tweet })
+    } catch (err) {
+      next()
+    }
   }
 }
-
 module.exports = tweetController
