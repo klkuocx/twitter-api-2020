348e5f69ba8552a35bd586da78518494f1ea495a
Kaleb Kuo <klkuo.cx@gmail.com>
2021-01-11 18:30:03 +0800

    refactor: update app.js to use socket.io

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
+// env init
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+// require server related packages
 const express = require('express')
-const routes = require('./routes')
 const bodyParser = require('body-parser')
 const passport = require('./config/passport')
 const cors = require('cors')
 const logger = require('morgan')
 
+// init server
 const app = express()
 const port = process.env.PORT
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 app.use(logger('dev'))
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(passport.initialize())
-routes(app)
 
-const server = require('http').createServer(app)
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    credentials: true
-  }
-})
-require('./utils/socket.io')(io)
-
+// run and listen to server 
 server.listen(port, () => {
   console.log(`KRLL Twitter API listening on port:${port}`)
 })
+
+require('./routes')(app)
+require('./utils/socket.io')(io)
 
 module.exports = app
