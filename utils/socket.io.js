2c319a763f9b372e6733511df54fe8986656e753
Kaleb Kuo <klkuo.cx@gmail.com>
2021-01-12 12:15:22 +0800

    feat: remove all socket setting and add authentication for getting onlineUser

--- utils/socket.io.js
+++ utils/socket.io.js
@@ -1,55 +1,12 @@
+// require middleware
+const { authenticateBySocket } = require('../middleware/auth')
+
+// define socket related variable
+const onlineUsers = {}
+
 module.exports = io => {
-  io.on('connection', (socket) => {
-    console.log('socket.io is connecting...')
-    // define chatroom variables
-    const users = {}
-    const typers = {}
-
-    // on connection status
-    socket.on('connect', (userId) => {
-      users[socket.id] = {
-        socketId: socket.id,
-        userId: userId
-      }
-      socket.broadcast.emit('connect', users[socket.id])
-    })
-
-    socket.on('disconnect', () => {
-      delete users[socket.id]
-    })
-
-    // on room events
-    socket.on('join', (room) => {
-      socket.join(room)
-      socket.to(room).emit('a new user has joined the room')
-    })
-
-    socket.on('leave', (room) => {
-      socket.leave(room)
-      socket.to(room).emit(`user ${socket.id} has left the room`)
-    })
-
-    // on message events
-    socket.on('typing', (room) => {
-      typers[socket.id] = 1
-      socket.broadcast.to(room).emit('typing', {
-        user: users[socket.id],
-        typers: Object.keys(typers).length
-      })
-    })
-
-    socket.on('stopped typing', (room) => {
-      delete typers[socket.id]
-      socket.broadcast.to(room).emit('stopped typing', Object.keys(typers).length)
-    })
-
-    socket.on('send message', (room, user, message) => {
-      delete typers[socket.id]
-      socket.broadcast.to(room).emit('send message', {
-        user: user,
-        message: message,
-        typers: Object.keys(typers).length
-      })
-    })
+  io.use(authenticateBySocket).on('connection', socket => {
+    const user = socket.request.user
+    console.log(`user(socket.id: ${socket.id}, userId: ${user.id}, name: ${user.name}) is connecting`)
   })
 }
