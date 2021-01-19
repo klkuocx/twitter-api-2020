a24cfc9b094bc9c606d288b9f01e4cf4e11db0b2
Kaleb Kuo <klkuo.cx@gmail.com>
2021-01-19 15:34:48 +0800

    feat: add user online event to socket server

--- utils/socket.io.js
+++ utils/socket.io.js
@@ -2,18 +2,7 @@
 // const { authenticateBySocket } = require('../middleware/auth')
 
 // define socket related variable
-/* onlineUsers = {
-  1: {
-    socketId: Set {sk1, sk2},
-    user: {
-      id: 1,
-      account: @user1,
-      name: user1,
-      ...
-    }
-  }
-} */
-// const onlineUsers = {}
+const onlineUsers = {} // {1:[sk1,sk2,...], 2:[...],...}
 
 module.exports = io => {
   // authenticate and get user
@@ -27,23 +16,25 @@ module.exports = io => {
     console.log(`socket.request: ${socket.request}`)
     console.log(`======Socket.IO======`)
 
-    // // join public room and add to onlineUsers
-    // socket.join('public room')
-    // if (Object.keys(onlineUsers).includes(user.id)) {
-    //   onlineUsers[user.id].socketIds.add(socket.id)
-    // } else {
-    //   onlineUsers[user.id] = {
-    //     socketIds: new Set(),
-    //     user: user
-    //   }
-    // }
-    // // update onlineUsers to frontend
-    // io.emit('onlineUsers', {
-    //   onlineUsers: onlineUsers,
-    //   countOfUsers: Object.keys(onlineUsers).length
-    // })
-    // // send message to all users in public room except sender
-    // socket.broadcast.to('public room').emit('joinRoom', `user: ${user.name} is online`)
+    //  when user is online
+    socket.on('online', userId => {
+      // join public room and add to onlineUsers
+      socket.join('publicRoom')
+      if (Object.keys(onlineUsers).includes(userId)) {
+        onlineUsers[userId].add(socket.id)
+      } else {
+        onlineUsers[userId] = new Set([socket.id])
+      }
+      console.log(`add socket in onlineUser[${userId}]`, onlineUsers)
+
+      // update onlineUsers to clients
+      io.emit('onlineUsers', {
+        onlineUsers: onlineUsers,
+        countOfUsers: Object.keys(onlineUsers).length
+      })
+      // send message to all users in public room except sender
+      socket.broadcast.to('publicRoom').emit('joinRoom', `user: ${userId} is online`)
+    })
 
     // // disconnect and leave public room
     // socket.on('disconnect', () => {
