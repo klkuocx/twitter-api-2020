fa0a5e400d7faec2ee0f9f674d6aaef90f7687de
Kaleb Kuo <klkuo.cx@gmail.com>
2021-01-19 16:44:01 +0800

    feat: add offline event to socket

--- utils/socket.io.js
+++ utils/socket.io.js
@@ -36,18 +36,26 @@ module.exports = io => {
       socket.broadcast.to('publicRoom').emit('joinRoom', `user: ${userId} is online`)
     })
 
-    // // disconnect and leave public room
-    // socket.on('disconnect', () => {
-    //   // remove socket.id from onlineUsers
-    //   onlineUsers[user.id].socketIds.delete(socket.id)
-    //   // update onlineUsers to frontend
-    //   io.emit('onlineUsers', {
-    //     onlineUsers: onlineUsers,
-    //     countOfUsers: Object.keys(onlineUsers).length
-    //   })
-    //   // send message to all users in public room except sender
-    //   socket.broadcast.to('public room').emit('leaveRoom', `user: ${user.name} is offline`)
-    // })
+    // disconnect and leave public room
+    socket.on('disconnect', () => {
+      // remove socket.id from onlineUsers
+      Object.keys(onlineUsers).forEach(userId => {
+        const userSet = onlineUsers[userId]
+        if (userSet.has(socket.id)) {
+          userSet.delete(socket.id)
+          console.info(`remove socket from onlineUser[${userId}]`, onlineUsers)
+          // update onlineUsers to client
+          io.emit('onlineUsers', {
+            onlineUsers: onlineUsers,
+            countOfUsers: Object.keys(onlineUsers).length
+          })
+        }
+        if (!userSet.size) {
+          // send message to all users in public room except sender
+          socket.broadcast.to('publicRoom').emit('leaveRoom', `user: ${userId} is offline`)
+        }
+      })
+    })
 
     // // user send message
     // socket.on('clientSendMessage', message => {
