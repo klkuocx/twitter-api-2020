a5d8240deecd2a2f95517986a250e5a88c8bca8c
Kaleb Kuo <klkuo.cx@gmail.com>
2021-02-01 13:17:48 +0800

    feat: add authenticateBySocket to socket server and get currentUser

--- utils/socket.io.js
+++ utils/socket.io.js
@@ -1,35 +1,37 @@
 // require middleware
-// const { authenticateBySocket } = require('../middleware/auth')
+const { authenticateBySocket } = require('../middleware/auth')
 
 // define socket related variable
-const onlineUsers = {} // {1:[sk1,sk2,...], 2:[...],...}
+const onlineUsers = {}
+/*
+{
+  1: user1Obj,
+  2: user2Obj,
+  ...
+}
+*/
+// onlineUsersArr = [ user1Obj, user2Obj, ... ]
 
 module.exports = io => {
   // authenticate and get user
-  // io.use(authenticateBySocket).on('connection', socket => {
-  //   const user = socket.request.user
-  //   console.log(`user(socket.id: ${socket.id}, userId: ${user.id}, name: ${user.name}) is connecting`)
-  io.on('connection', socket => {
+  io.use(authenticateBySocket).on('connection', socket => {
+    const currentUser = socket.request.user
     console.info(`======Socket.IO======`)
-    console.info(`socket.id: ${socket.id} is connecting...`)
-    console.info(`socket.conn: ${socket.conn}`)
-    console.info(`socket.request: ${socket.request}`)
+    console.log(`user(socket.id: ${socket.id}, userId: ${currentUser.id}, name: ${currentUser.name}) is connecting`)
     console.info(`======Socket.IO======`)
 
     //  when user is online
-    socket.on('online', userId => {
+    socket.on('online', () => {
       // join public room and add to onlineUsers
       socket.join('publicRoom')
-      if (Object.keys(onlineUsers).includes(userId)) {
-        onlineUsers[userId].add(socket.id)
-      } else {
-        onlineUsers[userId] = new Set([socket.id])
+      if (!Object.keys(onlineUsers).includes(currentUser.id)) {
+        onlineUsers[currentUser.id] = currentUser
       }
-      console.info(`add socket in onlineUser[${userId}]`, onlineUsers)
+      console.info(`add currentUser to onlineUser[${currentUser.id}]`, onlineUsers)
 
       // update onlineUsers to clients
       io.emit('onlineUsers', {
-        onlineUsers: onlineUsers,
+        onlineUsers: Object.values(onlineUsers),
         countOfUsers: Object.keys(onlineUsers).length
       })
       // send message to all users in public room except sender
@@ -38,40 +40,40 @@ module.exports = io => {
 
     // disconnect and leave public room
     socket.on('disconnect', () => {
-      // remove socket.id from onlineUsers
-      Object.keys(onlineUsers).forEach(userId => {
-        const userSet = onlineUsers[userId]
-        if (userSet.has(socket.id)) {
-          userSet.delete(socket.id)
-          console.info(`remove socket from onlineUser[${userId}]`, onlineUsers)
-          // update onlineUsers to client
-          io.emit('onlineUsers', {
-            onlineUsers: onlineUsers,
-            countOfUsers: Object.keys(onlineUsers).length
-          })
-        }
-        if (!userSet.size) {
-          // send message to all users in public room except sender
-          socket.broadcast.to('publicRoom').emit('leaveRoom', `user: ${userId} is offline`)
-        }
-      })
+      // remove currentUser from onlineUsers
+      if (Object.keys(onlineUsers).includes(currentUser.id)) {
+        delete onlineUsers[currentUser.id]
+        console.info(`remove currentUser from onlineUsers[${currentUser.id}]`, onlineUsers)
+
+        // update onlineUsers to client
+        io.emit('onlineUsers', {
+          onlineUsers: Object.values(onlineUsers),
+          countOfUsers: Object.keys(onlineUsers).length
+        })
+
+        // send message to all users in public room except sender
+        socket.broadcast.to('publicRoom').emit('leaveRoom', {
+          user: currentUser,
+          content: `user: ${currentUser.name} is offline`,
+          createdAt: new Date()
+        })
+      }
     })
 
     // when user send message
     socket.on('sendMessage', payload => {
-      const { userId, room, message } = payload
+      const { room, message } = payload
       if (!message.trim()) return
       socket.broadcast.to(room).emit('sendMessage', {
-        userId: userId,
+        user: currentUser,
         content: message.trim(),
         createdAt: new Date()
       })
     })
 
     // when user is typing
-    socket.on('typing', payload => {
-      const { userId, room } = payload
-      socket.broadcast.to(room).emit('typing', `user: ${userId} is typing`)
+    socket.on('typing', room => {
+      socket.broadcast.to(room).emit('typing', `user: ${currentUser.name} is typing`)
     })
   })
 }
